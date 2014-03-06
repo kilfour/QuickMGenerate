aa47e3ad640273668b52da45ee1ce3445e2547e7
kilfour <kilfour@gmail.com>
2014-03-06 01:18:24 +0100

    goofing around

---
+++ QuickMGenerate/Append.cs
@@ -0,0 +1,27 @@
+﻿using QuickMGenerate.UnderTheHood;
+
+namespace QuickMGenerate
+{
+	public static partial class MGen
+	{
+		public static Generator<State, string> Append(this Generator<State, string> generator, string appendix)
+		{
+			return
+				s =>
+					{  
+						var val = string.Format("{0}{1}", generator(s).Value, appendix);
+						return new Result<State, string>(val, s);
+					};
+		}
+
+		public static Generator<State, string> Append(this Generator<State, string> generator, Generator<State, string> appendix)
+		{
+			return
+				s =>
+				{
+					var val = string.Format("{0}{1}", generator(s).Value, appendix(s).Value);
+					return new Result<State, string>(val, s);
+				};
+		}
+	}
+}
--- QuickMGenerate/As.cs
+++ QuickMGenerate/As.cs
@@ -13,5 +13,15 @@ namespace QuickMGenerate
 						return new Result<State, object>(val, s);
 					};
 		}
+
+		public static Generator<State, string> AsString<T>(this Generator<State, T> generator)
+		{
+			return
+				s =>
+					{  
+						var val = generator(s).Value;
+						return new Result<State, string>(val.ToString(), s);
+					};
+		}
 	}
 }

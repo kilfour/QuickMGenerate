84976612faccf9e83cb1fc9e76c0a6b2a75c4ece
kilfour <kilfour@gmail.com>
2025-04-30 15:20:19 +0200

    ?

--- QuickMGenerate.Tests/GeneratingNamesSpike.cs
+++ QuickMGenerate.Tests/GeneratingNamesSpike.cs
@@ -30,9 +30,9 @@ namespace QuickMGenerate.Tests
 
 		public class Person
 		{
-			public string FirstName { get; set; }
-			public string LastName { get; set; }
-			public string Email { get; set; }
+			public string? FirstName { get; set; }
+			public string? LastName { get; set; }
+			public string? Email { get; set; }
 
 			public override string ToString()
 			{

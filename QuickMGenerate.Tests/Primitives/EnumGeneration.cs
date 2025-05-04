34c2264449ad1b8605e408d70d3dcfc2699493af
kilfour <kilfour@gmail.com>
2025-05-05 00:00:21 +0200

    test clean

--- QuickMGenerate.Tests/Primitives/EnumGeneration.cs
+++ QuickMGenerate.Tests/Primitives/EnumGeneration.cs
@@ -1,4 +1,6 @@
-﻿namespace QuickMGenerate.Tests.Primitives
+﻿using QuickMGenerate.Tests._Tools;
+
+namespace QuickMGenerate.Tests.Primitives
 {
 	[Enums(
 		Content = "Use `MGen.Enum<T>()`, where T is the type of Enum you want to generate. \n\nNo overload Exists.",
@@ -12,17 +14,8 @@
 			Order = 1)]
 		public void DefaultGenerator()
 		{
-			var generator = MGen.Enum<MyEnumeration>();
-			var one = false;
-			var two = false;
-			for (int i = 0; i < 10; i++)
-			{
-				var value = generator.Generate();
-				one = one || value == MyEnumeration.MyOne;
-				two = two || value == MyEnumeration.Mytwo;
-			}
-			Assert.True(one);
-			Assert.True(two);
+			CheckIf.TheseValuesAreGenerated(MGen.Enum<MyEnumeration>(),
+				MyEnumeration.MyOne, MyEnumeration.Mytwo);
 		}
 
 		[Fact]
@@ -31,17 +24,9 @@
 			Order = 2)]
 		public void Property()
 		{
-			var generator = MGen.One<SomeThingToGenerate>();
-			var one = false;
-			var two = false;
-			for (int i = 0; i < 10; i++)
-			{
-				var value = generator.Generate().AnEnumeration;
-				one = one || value == MyEnumeration.MyOne;
-				two = two || value == MyEnumeration.Mytwo;
-			}
-			Assert.True(one);
-			Assert.True(two);
+			CheckIf.TheseValuesAreGenerated(
+				MGen.One<SomeThingToGenerate>().Select(a => a.AnEnumeration),
+				MyEnumeration.MyOne, MyEnumeration.Mytwo);
 		}
 
 		[Fact]
@@ -50,21 +35,8 @@
 			Order = 3)]
 		public void NullableProperty()
 		{
-			var generator = MGen.One<SomeThingToGenerate>();
-			var isSomeTimesNull = false;
-			var isSomeTimesNotNull = false;
-			for (int i = 0; i < 50; i++)
-			{
-				var value = generator.Generate().ANullableProperty;
-				if (value.HasValue)
-				{
-					isSomeTimesNotNull = true;
-				}
-				else
-					isSomeTimesNull = true;
-			}
-			Assert.True(isSomeTimesNull);
-			Assert.True(isSomeTimesNotNull);
+			CheckIf.GeneratesNullAndNotNull(
+				MGen.One<SomeThingToGenerate>().Select(a => a.ANullableProperty));
 		}
 
 		[Fact]
@@ -82,11 +54,7 @@
 			public MyEnumeration? ANullableProperty { get; set; }
 		}
 
-		public enum MyEnumeration
-		{
-			MyOne,
-			Mytwo
-		}
+		public enum MyEnumeration { MyOne, Mytwo }
 
 		public class EnumsAttribute : ThePrimitiveGeneratorsAttribute
 		{
